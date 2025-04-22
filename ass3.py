eca0925e6454fbf971016ae8fd31fe8df7f204e5
Sidra776 <musabqureshi075@gmail.com>
2025-04-22 14:56:24 +0500

    Create ass3.py

---
+++ ass3.py
@@ -0,0 +1,6 @@
+Qno1: Write a program that calculate the sum, difference, product and average of three numbers 
+https://www.programiz.com/online-compiler/7SqUtcfla66Ew
+Qno2: Write a python code to calculate the area of following shapes
+https://www.programiz.com/online-compiler/2UmjO0FYhZScy
+Qno3: Write a program to swap values of two variables 
+https://www.programiz.com/online-compiler/7bi6doANRxoTO
